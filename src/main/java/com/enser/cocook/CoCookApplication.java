3129413dfb3d22f94342eefc4704a0ecfc4cb7f0
Casiopobre <ladysalvitachan@gmail.com>
2026-09-16 11:28:04 +0200

    Innitial commit

---
+++ src/main/java/com/enser/cocook/CoCookApplication.java
@@ -0,0 +1,13 @@
+package com.enser.cocook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoCookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoCookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/enser/cocook/CoCookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enser.cocook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoCookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
